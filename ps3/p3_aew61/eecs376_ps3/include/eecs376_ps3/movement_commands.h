72d1cdd2aa0643bb7c43219aad4c8b846c2208ff
aew61 <aew61@case.edu>
2017-02-16 16:05:47 -0500

    adding ps3...reformatting ps2 and adding lab2

---
+++ ps3/p3_aew61/eecs376_ps3/include/eecs376_ps3/movement_commands.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef EECS376_PS3_MOVEMENT_COMMANDS_H
+#define EECS376_PS3_MOVEMENT_COMMANDS_H
+
+// SYSTEM INCLUDES
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+// C++ PROJECT INCLUDES
+
+
+/**
+ * A function that sets all members of a geometry::Twist object to 0.0.
+ */
+geometry_msgs::Twist& zero_twist_command(geometry_msgs::Twist& cmd);
+
+/**
+ * A function that will publish a geometry::Twist object for a total amount
+ * of time in increments of dt. It is crucial that the rate of the ros::Rate
+ * object is 1.0 / dt.
+ */
+double move(double dt, double total_time, ros::Rate& timer,
+			ros::Publisher& publisher, geometry_msgs::Twist& cmd);
+
+/**
+ * A function that will zero out a geometry::Twist object, set the Twist object
+ * to represent turning about the z axis at rate of speed, for a certain amount
+ * of time in increments of dt. It is crucial that the rate of the ros::Rate
+ * object is 1.0 / dt.
+ */
+double rotate_about_z(double speed, double dt, double total_time, ros::Rate& timer, 
+					  ros::Publisher& publisher, geometry_msgs::Twist& cmd);
+
+/**
+ * A function that will zero out a geometry::Twist object, set the Twist object
+ * to represent moving forward in the x direction at rate of speed, for a certain
+ * amount of time in increments of dt. It is crucial that the rate of the
+ * ros::Rate object is 1.0 / dt.
+ */
+double move_forward_x(double speed, double dt, double total_time, ros::Rate& timer,
+					  ros::Publisher& publisher, geometry_msgs::Twist& cmd);
+
+#endif // end of EECS376_PS3_MOVEMENT_COMMANDS_H
---
+++ ps3/p3_aew61/eecs376_ps3/src/path_service.cc
@@ -0,0 +1,88 @@
+// SYSTEM INCLUDES
+#include <math.h>
+
+// ROS INCLUDES
+#include <ros/ros.h>
+#include <eecs376_ps3/PathSrv.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+
+
+// C++ PROJECT INCLUDES
+#include "eecs376_ps3/PathExecutor.h"
+#include "eecs376_ps3/movement_commands.h"
+
+
+const double linear_vel = 1.0; 	// meters / sec
+const double yaw_rate = 0.5;	// radians / sec
+const double sample_dt = 0.01;	// sample every 10Hz
+ros::Publisher global_publisher;
+
+
+bool path_callback(eecs376_ps3::PathSrvRequest& request, eecs376_ps3::PathSrvResponse& response)
+{
+	ROS_INFO("callback activated");
+    PathExecutor executor;
+
+   	unsigned int waypoints_size = request.nav_path.poses.size();
+    ROS_INFO("received path request with %u poses", waypoints_size);
+
+
+    std::vector<std::pair<double, double> > cmds = executor.generate_commands(request.nav_path.poses);
+    ros::Rate loop_timer(1.0 / sample_dt);
+    geometry_msgs::Twist twist_cmd;
+
+	double signed_speed = 0.0;
+    double total_time = 0.0;
+
+     // initally do nothing for 10 seconds
+    zero_twist_command(twist_cmd);
+    move_forward_x(0.0, sample_dt, 0.1, loop_timer, global_publisher, twist_cmd);
+
+    ROS_INFO("printing path: {");
+    for(unsigned int i = 0; i < cmds.size() && ros::ok(); ++i)
+    {
+    	ROS_INFO("\tcmd: {angle: %f, distance: %f}", cmds[i].first, cmds[i].second);
+    	
+    	// make sure to zero out the command first!
+    	zero_twist_command(twist_cmd);
+
+    	// take advantage of the fact that a cmd is either a rotation or a translation
+    	// (rotation = 0 if a translation, translation = 0 if a rotation)
+    	if(cmds[i].first != 0.0) // spin
+    	{
+    		signed_speed = (cmds[i].first < 0.0 ? -1.0 : 1.0) * yaw_rate;
+    		total_time = fabs(cmds[i].first) / yaw_rate;
+    		rotate_about_z(signed_speed, sample_dt, total_time, loop_timer, global_publisher, twist_cmd);
+    	}
+    	else // move forward
+    	{
+    		signed_speed = (cmds[i].second < 0.0 ? -1.0 : 1.0) * linear_vel;
+    		total_time = fabs(cmds[i].second) / linear_vel;
+    		move_forward_x(signed_speed, sample_dt, total_time, loop_timer, global_publisher, twist_cmd);
+    	}
+
+    	// publish nothing for 0.1 seconds
+    	zero_twist_command(twist_cmd);
+    	move_forward_x(0.0, sample_dt, 0.1, loop_timer, global_publisher, twist_cmd);
+
+
+    }
+    ROS_INFO("}");
+
+  	return true;
+}
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "path_service");
+	ros::NodeHandle handle;
+	global_publisher = handle.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 1);
+
+	ros::ServiceServer service = handle.advertiseService("path_service", path_callback);
+	ROS_INFO("Ready to accept paths");
+	ros::spin();
+
+	return 0;
+}
