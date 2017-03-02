c4c90517675171b9c17bb5e180c8d46d5b34b36a
aew61 <aew61@case.edu>
2017-03-02 13:14:19 -0500

    added lab 4 code

---
+++ lab_4/include/lab_4/waypoint_generator.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef LAB_4_WAYPOINT_GENERATOR_H
+#define LAB_4_WAYPOINT_GENERATOR_H
+
+// SYSTEM INCLUDES
+
+
+// ROS INCLUDES
+#include <geometry_msgs/Pose.h>
+
+
+// C++ PROJECT INCLUDES
+
+/**
+ * This function takes a pair of x, y coordinates and an angle phi about the z axis, and comverts
+ * them into a valid PoseStamped message. This message is intended for use in a list of waypoints
+ * a "nav_msgs/Path" message, but theoretically should be valid for any 2D geometry.
+ */
+geometry_msgs::Pose generate_waypoint(const double x, const double y, const double phi);
+
+#endif // end of LAB_4_WAYPOINT_GENERATOR_H
---
+++ lab_4/src/waypoint_generator.cc
@@ -0,0 +1,25 @@
+// SYSTEM INCLUDES
+#include <math.h>
+
+// ROS INCLUDES
+#include <geometry_msgs/Quaternion.h>
+
+// C++ PROJECT INCLUDES
+#include "lab_4/waypoint_generator.h"
+
+
+geometry_msgs::Pose generate_waypoint(const double x, const double y, const double phi)
+{
+	geometry_msgs::Pose pose;
+
+	pose.position.x = x;
+	pose.position.y = y;
+	pose.position.z = 0.0;
+
+	pose.orientation.x = 0.0;
+	pose.orientation.y = 0.0;
+	pose.orientation.z = sin(phi / 2.0);
+	pose.orientation.w = cos(phi / 2.0);
+
+	return pose;
+}
